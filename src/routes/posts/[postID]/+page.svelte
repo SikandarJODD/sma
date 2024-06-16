477fb19e103933cece262ec42999d5fcc45c8bec
SikandarJODD <vu1f2021072@pvppcoe.ac.in>
2024-06-16 09:42:46 +0530

    SEO Single Post

--- src/routes/posts/[postID]/+page.svelte
+++ src/routes/posts/[postID]/+page.svelte
@@ -5,4 +5,32 @@
   let postDetails = data.postDetails;
 </script>
 
+<!-- SEO Single Post -->
+<svelte:head>
+  <title>
+    {postDetails.title}
+  </title>
+  <meta
+    name="description"
+    content="Post is created by {postDetails.social_name}"
+  />
+
+  <meta property="og:title" content={postDetails.title} />
+  <meta property="og:image" content={postDetails.image} />
+  <meta property="og:site_name" content="Social-Site" />
+  <meta property="og:url" content="https://social-site-buddy.vercel.app" />
+  <meta
+    property="og:description"
+    content="Post is created by {postDetails.social_name}"
+  />
+
+  <!-- Twitter SEO -->
+  <meta property="twitter:image" content={postDetails.image} />
+  <meta property="twitter:card" content="summary_large_image" />
+  <meta property="twitter:title" content={postDetails.title} />
+  <meta
+    property="twitter:description"
+    content="Post is created by {postDetails.social_name}"
+  />
+</svelte:head>
 <SinglePost singlePost={postDetails} />
